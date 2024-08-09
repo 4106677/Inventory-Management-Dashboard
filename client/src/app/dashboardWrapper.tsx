e9107170883df0aee990a75b9ee6cf0e7e90914e
Yaroslav <4106677@gmail.com>
2024-08-09 11:46:40 +0300

    start project

---
+++ client/src/app/dashboardWrapper.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const DashboardWrapper = ({children} : {children: React.ReactNode}) => {
+    return (
+        <div>
+            DashboardWrapper
+            {children}
+        </div>
+    );
+};
+
+export default DashboardWrapper;

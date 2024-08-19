09efc09787ecab5b5eb7bcb47cab3ab0230b7d68
Yaroslav <4106677@gmail.com>
2024-08-19 18:01:55 +0300

    add dashboard

---
+++ server/src/index.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import dashboardRoutes from "./routes/dashboardRoutes";
+/* ROUTE IMPORTS */
+
+/* CONFIGURATIONS */
+dotenv.config();
+const app = express();
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}))
+app.use(morgan("common"))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
+
+/* ROUTES */
+// app.get("/hello", (req: express.Request, res: express.Response) => {
+// 	res.send("Hello World!");
+// })
+app.use("/dashboard", dashboardRoutes)
+
+/* SERVER */
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+	console.log(`Server started on port ${port}`);
+})

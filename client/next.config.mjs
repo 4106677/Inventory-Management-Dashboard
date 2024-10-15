95553f18aaa1ba0dc00834003cf64ce336ed612c
Yaroslav <4106677@gmail.com>
2024-10-15 15:05:36 +0300

    add port

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -3,9 +3,10 @@ const nextConfig = {
 	images: {
 		remotePatterns: [
 			{
-				protocol: 'https',
-				hostname: 's3-inventorymanagement-api.s3.eu-north-1.amazonaws.com',
-				pathname: '/**',
+				protocol: "https",
+				hostname: "s3-inventorymanagement-api.s3.eu-north-1.amazonaws.com",
+				port: "",
+				pathname: "/**",
 			},
 		],
 	},
